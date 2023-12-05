af3db91b93bae4b84c790bdc0b969a836a10bf9c
henrico <henrico.an@gmail.com>
2023-12-05 10:33:02 -0300

    docs/TASK-0016: updating volunteers images

--- src/data/VolunteersData.js
+++ src/data/VolunteersData.js
@@ -9,12 +9,22 @@
  * @property (string) linkedin - volunteer's linkedin.
  */
 
+import graceImg from '../../public/volunteers/avatar_GK.jpg'
+import caioImg from '../../public/volunteers/caio-avatar2.jpg'
+import cristianImg from '../../public/volunteers/cristian_avatar.png'
+import evandroImg from '../../public/volunteers/evandro-avatar.jpg'
+import henricoImg from '../../public/volunteers/henrico-avatar.jpg'
+import adrianoImg from '../../public/volunteers/tech-avatar.jpg'
+import thalytaImg from '../../public/volunteers/thalyta_avatar2.png'
+import villaImg from '../../public/volunteers/villa-avatar.jpg'
+import ylderlanImg from '../../public/volunteers/ylderlan_avatar.png'
+
 const volunteers = [
   {
     id: crypto.randomUUID(),
     name: 'Adriano Souza',
-    picture: 'https://github.com/silvasouzaadriano.png',
-    qualification: '',
+    picture: adrianoImg,
+    qualification: 'loading...',
     participation: 'Tech Lead',
     github: 'https://github.com/silvasouzaadriano',
     linkedin: 'https://www.linkedin.com/in/adriano-souza-9b1a1b11/',
@@ -22,8 +32,8 @@ const volunteers = [
   {
     id: crypto.randomUUID(),
     name: 'Grace Kelly',
-    picture: 'https://github.com/gksouza.png',
-    qualification: '',
+    picture: graceImg,
+    qualification: 'loading...',
     participation: 'PM | PO | QA Manager | QA Tester',
     github: 'https://github.com/gksouza',
     linkedin: 'https://www.linkedin.com/in/gkos22/',
@@ -31,8 +41,8 @@ const volunteers = [
   {
     id: crypto.randomUUID(),
     name: 'Caio Balduino',
-    picture: 'https://github.com/Caiobaldur.png',
-    qualification: '',
+    picture: caioImg,
+    qualification: 'loading...',
     participation: 'Web Designer e Dev Front End',
     github: 'https://github.com/Caiobaldur',
     linkedin: 'https://www.linkedin.com/in/caio-balduino/',
@@ -40,8 +50,8 @@ const volunteers = [
   {
     id: crypto.randomUUID(),
     name: 'Cristian Sbardelotto',
-    picture: 'https://github.com/cristian-sbardelotto.png',
-    qualification: '',
+    picture: cristianImg,
+    qualification: 'loading...',
     participation: 'Dev FrontEnd',
     github: 'https://github.com/cristian-sbardelotto',
     linkedin: 'https://www.linkedin.com/in/cristian-k-sbardelotto/',
@@ -49,8 +59,8 @@ const volunteers = [
   {
     id: crypto.randomUUID(),
     name: 'Evandro Damaso',
-    picture: 'https://github.com/dam450.png',
-    qualification: '',
+    picture: evandroImg,
+    qualification: 'loading...',
     participation: 'Squad Lead e Dev Front End',
     github: 'https://github.com/dam450',
     linkedin: 'https://www.linkedin.com/in/evandro-damaso/',
@@ -58,17 +68,26 @@ const volunteers = [
   {
     id: crypto.randomUUID(),
     name: 'Francisco Ylderlan',
-    picture: 'https://github.com/FranciscoYlderlan.png',
-    qualification: '',
+    picture: ylderlanImg,
+    qualification: 'loading...',
     participation: 'Squad Lead e Dev Front End',
     github: 'https://github.com/FranciscoYlderlan',
     linkedin: 'https://www.linkedin.com/in/franciscoylderlanoliveira/',
   },
   {
     id: crypto.randomUUID(),
+    name: 'Henrico Angolera',
+    picture: henricoImg,
+    qualification: 'loading...',
+    participation: 'QA Tester e Dev Front End',
+    github: 'https://github.com/HenricoAngolera',
+    linkedin: 'https://www.linkedin.com/in/henrico-angolera/',
+  },
+  {
+    id: crypto.randomUUID(),
     name: 'Mateus Villa Nova',
-    picture: 'https://github.com/Villa-Nova.png',
-    qualification: '',
+    picture: villaImg,
+    qualification: 'loading...',
     participation: 'QA Tester e Dev Front End',
     github: 'https://github.com/Villa-Nova',
     linkedin: 'https://www.linkedin.com/in/mateusvillanova/',
@@ -76,8 +95,8 @@ const volunteers = [
   {
     id: crypto.randomUUID(),
     name: 'Thalyta Rangel',
-    picture: 'https://github.com/ThalytaRangel.png',
-    qualification: '',
+    picture: thalytaImg,
+    qualification: 'loading...',
     participation: 'Dev Front End',
     github: 'https://github.com/ThalytaRangel',
     linkedin: 'https://www.linkedin.com/in/thalyta-rangel-341b44161/',
