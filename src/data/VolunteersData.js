c7e93c4498fa142e2cb64973807f1d0521f39d40
Mateus Villa Nova <villa1999nova@gmail.com>
2024-05-22 16:44:01 -0300

    fix/LBLK-16: Removing duplicate data

--- src/data/VolunteersData.js
+++ src/data/VolunteersData.js
@@ -100,16 +100,6 @@ const volunteers = [
   },
   {
     id: crypto.randomUUID(),
-    name: 'Matheus Hofstede',
-    picture: '/volunteers/hofs-avatar.png',
-    qualification:
-      'Pontualidade é uma entre tantas características fortes da sua persona tech. Seu conhecimento técnico é exponencial e ímpar, com altíssimo nível de liderança e motivador nato. Mentor em AWS, resoluções e sugestões em refatoração de componentes e repositórios.',
-    participation: 'Dev Back End',
-    github: 'https://github.com/hofstede-matheus/',
-    linkedin: 'https://www.linkedin.com/in/hofstede-matheus/',
-  },
-  {
-    id: crypto.randomUUID(),
     name: 'Mateus Villa Nova',
     picture: '/volunteers/villa-avatar.jpg',
     qualification:
